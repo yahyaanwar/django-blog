3a3477c66069fca3c28c9e8328c14defabbc6dba
Yahya Anwar <yahya.anw@gmail.com>
2019-06-28 13:21:53 +0700

    [IMP] Config and adding static assets

--- dk_blog/settings.py
+++ dk_blog/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'assets')
