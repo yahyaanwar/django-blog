60352a8bd8a151f5121e236071628c787eb8d406
Yahya Anwar <yahya.anw@gmail.com>
2019-06-29 10:45:00 +0700

    [IMP] Adding custom user

--- user/views.py
+++ user/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout
 
+from .forms import CustomUserCreationForm
+
 # Create your views here.
 
 def index(request):
@@ -22,13 +24,13 @@ def signin(request):
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('user:index')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'user/signup.html', {'form': form})
 
 def signout(request):
