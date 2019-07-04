139f4d9c45cddb320a3ae79226d0f680a77cd648
Yahya Anwar <yahya.anw@gmail.com>
2019-07-04 15:55:19 +0700

    [FIX] Fix assets structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,18 @@
-$(document).ready(function(){
-    $('input[name="title"]').focusout(function(){
+$(document).ready(function () {
+    $('input[name="title"]').focusout(function () {
         var title = $(this).val();
-        if($('input[name="slug"]').attr('edit') != 'true')
+        if ($('input[name="slug"]').attr('edit') != 'true')
             $('input[name="slug"]').val(convertToSlug(title))
     })
 
-    $('input[name="slug"]').keypress(function(){
-        $(this).attr('edit','true')
+    $('input[name="slug"]').keypress(function () {
+        $(this).attr('edit', 'true')
     })
 })
-s
-function convertToSlug(text)
-{
+
+function convertToSlug(text) {
     return text
         .toLowerCase()
-        .replace(/ /g,'-')
-        .replace(/[^\w-]+/g,'')
-        ;
+        .replace(/ /g, '-')
+        .replace(/[^\w-]+/g, '');
 }
