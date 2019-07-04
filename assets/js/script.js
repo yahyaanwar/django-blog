43f04cf8997ccfdc1d49f2544912acf803dd3272
haris <harisrizavalevi@gmail.com>
2019-07-04 23:58:58 +0700

    [IMP] update and delete post

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 $(document).ready(function () {
     $('input[name="title"]').focusout(function () {
-        var title = $(this).val();
+        var title = $(this).val()
         if ($('input[name="slug"]').attr('edit') != 'true')
             $('input[name="slug"]').val(convertToSlug(title))
     })
 
     $('input[name="slug"]').keypress(function () {
         $(this).attr('edit', 'true')
+    })
+
+    $('.btnDeletePost').click(function () {
+        var title = $(this).attr('title')
+        var url = $(this).attr('url')
+        if (confirm('Apakah anda yakin akan menghapus Post '+title+'?')) {
+            window.location.replace(url)
+        }
     })
 })
 
--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,10 @@ app_name = 'blog'
 urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create, name="create"),
+    path("<slug:slug>/update/", views.update, name="update"),
+    path("<slug:slug>/delete/", views.delete, name="delete"),
     path("<slug:slug>/", views.post_detail, name="post_detail"),
+    
+    
+   
 ]
